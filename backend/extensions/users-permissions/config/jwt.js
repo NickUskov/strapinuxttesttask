830a4384dab1c673654fb416f12bfec02056ae66
Nick Uskov <dargez.rogue@gmail.com>
2021-03-01 14:18:45 +0500

    first commit

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd8a2546f-763b-419d-b91c-6d18495d9006'
+};
---
+++ frontend/pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <h1>Список ресторанов</h1>
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <ul v-else>
+      <li v-for="framework in frameworks" :key="framework.id">
+        {{ framework.name }}
+        <br>
+        <small>
+          {{ framework.description }}
+        </small>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from "../.nuxt/axios";
+
+  export default {
+    name: 'App',
+    data () {
+      return {
+        frameworks: [],
+        error: null
+      }
+    },
+    async mounted () {
+      try {
+        this.frameworks = await this.$axios.$get('http://localhost:1337/frameworks')
+      } catch (error) {
+        this.error = error
+      }
+    }
+  }
+</script>
+
+<style>
+  ul {
+    padding: 0;
+  }
+  li {
+    list-style: none;
+    margin-bottom: 10px;
+    border-bottom: 1px solid gray;
+  }
+  h1 {
+    margin-bottom: 1rem;
+  }
+  .container {
+    padding: 1rem;
+  }
+</style>
